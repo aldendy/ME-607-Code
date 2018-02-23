f7444398b164b30e8e4f107c0e3ec01ea55e3000
aldendy <36775441+aldendy@users.noreply.github.com>
2018-02-23 15:14:27 -0700

    2D Integrand Test and 1D Improvements
    I added a 2D test for the integrand and improved the 1D test to take into account all of the elemental indices 'a' and 'b' for the B matrices.

--- Assgn_8Test.py
+++ Assgn_8Test.py
@@ -16,11 +16,26 @@ class BasicTest(unittest.TestCase):
         dims = 1
         intpt = 0
         xa = [[0, 0, 0], [1, 0, 0]]
+        # Next, we test for all combinations of 'a' and 'b'
+        for i in range(2):  # for every 'a' or local element #...
+            for j in range(2):  # for every 'b'...
+                kab = getEnergyDensity(dims, intpt, xa, i, j)
+                self.assertAlmostEqual(kab, (-1)**(i+j)*200e9)
+
+    # next, we test in 2D
+    def test_getEnDen2D(self):
+        dims = 2
+        intpt = 0
+        xa = [[0, 0, 0], [1, 0, 0], [0, 1, 0], [1, 1, 0]]
         a = 0
         b = 0
 
         kab = getEnergyDensity(dims, intpt, xa, a, b)
-        self.assertAlmostEqual(kab, 200e9)
+        # come back to this answer and check
+        correct = [[1.84551963e+11, 8.88583526e+10], [8.88583526e+10, 1.84551963e+11]]
+        for i in range(len(kab)):  # for every row...
+            for j in range(len(kab)):  # for every column...
+                self.assertAlmostEqual(kab[i][j]/correct[i][j], 1)
 
 ###################################################################
 
