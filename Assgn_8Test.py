aa304d7d7df4a16f3a027ef1a17569a547d81b2d
aldendy <36775441+aldendy@users.noreply.github.com>
2018-02-23 14:43:54 -0700

    Test File for 8
    We add a mostly blank file for performing testing on Assignment 8

---
+++ Assgn_8Test.py
@@ -0,0 +1,8 @@
+# This file implements all necessary tests for the code in Assignment_8
+
+
+import unittest
+from Assignment_8 import *
+
+
+###################################################################
